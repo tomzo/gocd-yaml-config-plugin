c234aafcc3124d254a4f0444d5ed358214a164e6
Tomasz Setkowski <tom@ai-traders.com>
2016-07-16 08:00:24 +0000

    added environments

--- src/main/java/cd/go/plugin/config/yaml/transforms/EnvironmentsTransform.java
+++ src/main/java/cd/go/plugin/config/yaml/transforms/EnvironmentsTransform.java
@@ -1,17 +1,46 @@
 package cd.go.plugin.config.yaml.transforms;
 
+import cd.go.plugin.config.yaml.YamlConfigException;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.Map;
 
+import static cd.go.plugin.config.yaml.YamlUtils.addOptionalStringList;
+import static cd.go.plugin.config.yaml.transforms.EnvironmentVariablesTransform.JSON_ENV_VAR_FIELD;
+
 public class EnvironmentsTransform {
+    private static final String JSON_ENV_NAME_FIELD = "name";
     private EnvironmentVariablesTransform environmentVariablesTransform;
 
     public EnvironmentsTransform(EnvironmentVariablesTransform environmentVariablesTransform){
         this.environmentVariablesTransform = environmentVariablesTransform;
     }
 
-    public JsonElement transform(Map.Entry<String, Object> env) {
-        return  null;
+    public JsonObject transform(Object yamlObject) {
+        Map<String,Object> map = (Map<String,Object>)yamlObject;
+        for(Map.Entry<String, Object> entry : map.entrySet()) {
+            return transform(entry);
+        }
+        throw new RuntimeException("expected environments hash to have 1 item");
+    }
+
+    public JsonObject transform(Map.Entry<String, Object> env) {
+        String envName = env.getKey();
+        JsonObject envJson = new JsonObject();
+        envJson.addProperty(JSON_ENV_NAME_FIELD,envName);
+        Object envObj = env.getValue();
+        if("".equals(envObj))
+            return envJson;
+        if(!(envObj instanceof Map))
+            throw new YamlConfigException("Expected environment to be a hash");
+        Map<String, Object> envMap = (Map<String,Object>)envObj;
+        addOptionalStringList(envJson,envMap,"agents","agents");
+        addOptionalStringList(envJson,envMap,"pipelines","pipelines");
+        JsonArray jsonEnvVariables = environmentVariablesTransform.transform(envMap);
+        if(jsonEnvVariables != null && jsonEnvVariables.size() > 0)
+            envJson.add(JSON_ENV_VAR_FIELD,jsonEnvVariables);
+        return envJson;
     }
 }
---
+++ src/test/java/cd/go/plugin/config/yaml/transforms/EnvironmentsTransformTest.java
@@ -0,0 +1,41 @@
+package cd.go.plugin.config.yaml.transforms;
+
+import cd.go.plugin.config.yaml.JsonObjectMatcher;
+import com.google.gson.JsonObject;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static cd.go.plugin.config.yaml.TestUtils.readJsonObject;
+import static cd.go.plugin.config.yaml.TestUtils.readYamlObject;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class EnvironmentsTransformTest {
+    private final EnvironmentsTransform parser;
+
+    public EnvironmentsTransformTest(){
+        parser = new EnvironmentsTransform(new EnvironmentVariablesTransform());
+    }
+
+    @Test
+    public void shouldTransformCompleteEnvironment() throws IOException {
+        testTransform("complete");
+    }
+
+    @Test
+    public void shouldTransformEmptyEnvironment() throws IOException {
+        testTransform("empty");
+    }
+
+    private JsonObject testTransform(String caseFile) throws IOException {
+        return testTransform(caseFile,caseFile);
+    }
+
+    private JsonObject testTransform(String caseFile,String expectedFile) throws IOException {
+        JsonObject expectedObject = (JsonObject)readJsonObject("parts/environments/" + expectedFile + ".json");
+        JsonObject jsonObject = parser.transform(readYamlObject("parts/environments/" + caseFile + ".yaml"));
+        assertThat(jsonObject,is(new JsonObjectMatcher(expectedObject)));
+        return jsonObject;
+    }
+}

9824022fd61524010db90a760a5a8f9bd7669cce
Tomasz Setkowski <tom@ai-traders.com>
2017-09-15 08:24:57 +0000

    improve release tasks

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# 0.5.0 (Unreleased)
+
+ * new release build tasks
+
 # 0.4.0 (16 Dec 2016)
 
  * fixed IO error reporting
--- release.sh
+++ release.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-source token.sh
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "GITHUB_TOKEN is unset";
+    exit 1;
+fi
+
 
 if [ ! -f linux-amd64-github-release.tar.bz2 ]; then
     wget https://github.com/aktau/github-release/releases/download/v0.6.2/linux-amd64-github-release.tar.bz2 -O linux-amd64-github-release.tar.bz2
 fi
 tar xf linux-amd64-github-release.tar.bz2
 
-VERSION="0.4.0"
+VERSION=$(ls build/libs/yaml-config-plugin-*.jar | grep -Eo '[0-9]+\.[0-9]+\.[0-9]+')
 GHRELEASE_BIN="./bin/linux/amd64/github-release"
 
 $GHRELEASE_BIN release \
@@ -22,4 +26,4 @@ $GHRELEASE_BIN upload \
   --repo gocd-yaml-config-plugin \
   --tag $VERSION \
   --name "yaml-config-plugin-$VERSION.jar" \
-  --file build/libs/yaml-config-plugin-bundle-$VERSION.jar
+  --file build/libs/yaml-config-plugin-$VERSION.jar
